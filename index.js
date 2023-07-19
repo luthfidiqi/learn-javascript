841fbecc88845bf85461939e7f925374c99f8dd1
luthfidiqi <luthfidiqi@gmail.com>
2023-07-19 11:51:34 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Using Lenght
+let input = prompt("Input Character in here :");
+let maxInput = 100;
+
+inputCurrent = input.length;
+inputLeft = maxInput - inputCurrent;
+
+console.log(`You have written ${inputCurrent}, you have -${inputLeft} characters left`);
+
