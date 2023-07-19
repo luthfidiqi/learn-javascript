8261613333bd5129359132c4d8225bcd57b6bdf5
luthfidiqi <luthfidiqi@gmail.com>
2023-07-19 12:52:19 +0700

    slice

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // Using Lenght
 let input = prompt("Input Character in here :");
-let maxInput = 100;
+let maxInput = 10;
+inputLeft = maxInput - input.length;
 
-inputCurrent = input.length;
-inputLeft = maxInput - inputCurrent;
+console.log(`You have written ${input.length}, you have ${inputLeft} characters left`);
 
-console.log(`You have written ${inputCurrent}, you have -${inputLeft} characters left`);
+// Slice
+console.log(`Your input is : ${input.slice(0,10)}`);
 
+// if else statement
+if(input.length < maxInput) {
+    alert("Data succesfully input")
+} else {
+    alert(`Your characters is ${input.length} that was more than max input, please delete ${inputLeft} characters`)
+}; 
